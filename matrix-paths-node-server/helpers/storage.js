02526ea1c0843281315ba4c9750bbeb25bbdcfa0
mario-leme <mariolemesmedina@gmail.com>
2018-10-31 05:43:48 +0000

    B: Added env var to get path to store files

--- matrix-paths-node-server/helpers/storage.js
+++ matrix-paths-node-server/helpers/storage.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 exports.storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, path.join('files'));
+    cb(null, process.env.FILES_PATH);
   },
   filename: function(req, file, cb) {
     cb(null, file.originalname);
